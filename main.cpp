326d43d0ba240a6eee8e52a3579b7418e3d919f3
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-12 02:26:09 -0300

    random generator ready

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
+#include <iomanip>
 #include <iostream>
-#include "gmp.h"
+#include <random>
+#include "gmpxx.h"
 
 int main(void) {
 	using namespace std;
-	mpz_t sum;
-	mpz_init(sum);
-	mpz_set_str(sum, "12345", 10);
+	// precision ;)
+	const int kP = 90000;
+
+	// using Mersene Twister engine
+	gmp_randclass r1(gmp_randinit_mt);
+
+	// seed providade by a random device (implementation defined)
+	r1.seed(random_device{}());
+	cout << setprecision(kP);
+
+	cout << r1.get_f(kP) << endl;
+
+	mpf_t sum;
+	mpf_init(sum);
+	mpf_set_str(sum, "12345", 10);
 	cout << sum << endl;
-	mpz_clear(sum);
+	mpf_clear(sum);
 	int i{};
 	cout << i << endl;
 	return 0;
