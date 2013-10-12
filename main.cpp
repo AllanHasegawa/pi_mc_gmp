2eba6357995947eaf9ef92660ae7a137f8f0ae14
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-10-12 03:55:40 -0300

    now powered by c++11 multithreading; also, can't slpel "presicion'

--- main.cpp
+++ main.cpp
@@ -1,36 +1,65 @@
 #include <iomanip>
 #include <iostream>
+#include <vector>
 #include <random>
+#include <future>
+#include <algorithm>
 #include "gmpxx.h"
 
-int main(void) {
-	using namespace std;
-	// precision ;)
-	const int kPresicionBits = 512;
-	const int kSamples = 9000000;
-
+long parallelF(const int precision, const long samples) {
 	// using Mersene Twister engine
 	gmp_randclass r1(gmp_randinit_mt);
 
 	// seed providade by a random device (implementation defined)
-	r1.seed(random_device{}());
+	r1.seed(std::random_device{}());
 
-	mpf_class x(0, kPresicionBits);
-	mpf_class y(0, kPresicionBits);
+	mpf_class x(0, precision);
+	mpf_class y(0, precision);
 	int counter{};
-	for (int i = 0; i < kSamples; ++i) {
+	for (int i = 0; i < samples; ++i) {
+		//from GMP doc:
+		//"if prec is not given then to the precision of the destination"
 		x = r1.get_f();
 		y = r1.get_f();
 		x = x*x;
 		y = y*y;
-		if (x+y < 1) ++counter;
+		if (x+y <= 1) ++counter;
+	}
+	return counter;
+}
+
+
+int main(void) {
+	using namespace std;
+	// precision ;)
+	const int kPrecisionBits = 512;
+	const int kNThreads = 16;
+	// divide the work equally between each thread
+	const long kSamples = 90000000 * kNThreads;
+
+	vector<future<long>> asyncsHandler;
+
+	for (int i = 0; i < kNThreads; ++i) {
+		asyncsHandler.push_back(
+			async(std::launch::async,
+				[]{ return parallelF(kPrecisionBits, kSamples/kNThreads); }
+			));
 	}
 
-	mpf_class pi(0, kPresicionBits);
+
+	long counter{};
+	for_each(asyncsHandler.begin(), asyncsHandler.end(),
+		[&counter](future<long>& result) {
+			counter += result.get();
+		});
+
+	mpf_class pi(0, kPrecisionBits);
 	pi = counter;
+
+	// pi = (counter*4)/samples
 	pi = pi*4;
-	pi = pi/kSamples;
-	
+	pi = pi/(kSamples);
+
 	cout << setprecision(200);
 	cout << pi << endl;
 
